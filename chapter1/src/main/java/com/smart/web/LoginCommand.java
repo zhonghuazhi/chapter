5c83d0fedba95a68d899478fb4c075e40db81e2d
xiaomo <307924323@qq.com>
2016-04-03 08:48:08 +0800

    chapter init

---
+++ chapter1/src/main/java/com/smart/web/LoginCommand.java
@@ -0,0 +1,33 @@
+package com.smart.web;
+
+/**
+ * Created by zhonghua on 16/4/3.
+ */
+public class LoginCommand {
+
+    /**
+     *
+     */
+    private String userName;
+
+    /**
+     *
+     */
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
